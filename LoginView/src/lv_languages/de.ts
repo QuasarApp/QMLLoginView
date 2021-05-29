acc385d2a82f4604735be67e79b80c0be22cf45b
Oleg-designer <sweetletal0@gmail.com>
2021-05-29 12:08:49 +0300

    ref #23 Add languages zh de fr es pl and translations

---
+++ LoginView/src/lv_languages/de.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+<context>
+    <name>LoginView</name>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="21"/>
+        <source>Create a new account</source>
+        <translation>Ein neues Konto erstellen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="21"/>
+        <location filename="../LoginViewModule/LoginView.qml" line="261"/>
+        <source>LogIn</source>
+        <translation>Anmeldung</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="22"/>
+        <source>Please fill this form to create an account</source>
+        <translation>Bitte füllen Sie dieses Formular aus, um ein Konto zu erstellen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="23"/>
+        <source>Please fill this form to login in your account</source>
+        <translation>Bitte füllen Sie dieses Formular aus, um sich in Ihrem Konto anzumelden</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="66"/>
+        <source>First Name</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="69"/>
+        <source>Empy or incorrect name. Please enter your name.</source>
+        <translation>Leerer oder falscher Name. Bitte geben Sie Ihren Namen ein.</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="84"/>
+        <source>Last Name</source>
+        <translation>Nachname</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="111"/>
+        <source>Select you country</source>
+        <translation>Wählen Sie Ihr Land aus</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="133"/>
+        <source>EMail</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="137"/>
+        <source>Empty or incorrect email address. Please enter your name.</source>
+        <translation>Leere oder falsche E-Mail-Adresse. Bitte geben Sie Ihren Namen ein.</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="152"/>
+        <source>Nickname</source>
+        <translation>Spitzname</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="156"/>
+        <source>Empty or incorrect Nickname. Please enter your name.</source>
+        <translation>Leerer oder falscher Spitzname. Bitte geben Sie Ihren Namen ein.</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="178"/>
+        <source>password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="192"/>
+        <source>Confirm password</source>
+        <translation>Kennwort bestätigen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="204"/>
+        <source>Forgot password</source>
+        <translation>Passwort vergessen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="225"/>
+        <source>I accept the Terms of use</source>
+        <translation>Ich akzeptiere die Nutzungsbedingungen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="232"/>
+        <source>Show terms of use</source>
+        <translation>Nutzungsbedingungen anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="260"/>
+        <source>SignUp</source>
+        <translation>Anmeldung</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="286"/>
+        <source>Login In</source>
+        <translation>Einloggen in</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="289"/>
+        <source>Sign Up</source>
+        <translation>Anmeldung</translation>
+    </message>
+    <message>
+        <location filename="../LoginViewModule/LoginView.qml" line="353"/>
+        <source>Password must match the 1st field and the password cannot be empty</source>
+        <translation>Das Passwort muss mit dem 1. Feld übereinstimmen und das Passwort darf nicht leer sein</translation>
+    </message>
+</context>
+<context>
+    <name>LoginView::LVMainModel</name>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="204"/>
+        <source>must be longer than 8 characters</source>
+        <translation>muss länger als 8 Zeichen sein</translation>
+    </message>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="208"/>
+        <source>must contain numbers</source>
+        <translation>muss Zahlen enthalten</translation>
+    </message>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="212"/>
+        <source>must contain small chars</source>
+        <translation>muss kleine Zeichen enthalten</translation>
+    </message>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="216"/>
+        <source>must contain large chars</source>
+        <translation>muss große Zeichen enthalten</translation>
+    </message>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="220"/>
+        <source>must contain extra chars (!@#$%^&amp;*)</source>
+        <translation>muss zusätzliche Zeichen enthalten (!@#$%^&amp;*)</translation>
+    </message>
+    <message>
+        <location filename="../lvmainmodel.cpp" line="223"/>
+        <source>Password requirements: </source>
+        <translation>Passwortanforderungen:</translation>
+    </message>
+</context>
+</TS>
